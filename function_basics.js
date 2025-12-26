7e405edbc482b8e2041dca218745aa8754ba4443
Amit verma <190012191+amitskverma@users.noreply.github.com>
2025-12-26 09:41:23 +0530

    basic function

---
+++ function_basics.js
@@ -0,0 +1,29 @@
+// Write functions for:
+// Add two numbers
+// Subtract two numbers
+// Check if a number is even
+
+
+function add(a,b){
+    let c=a+b;
+console.log("Addition",c)
+
+if(c%2==0){
+    console.log("even")
+}
+else console.log("odd")
+}
+
+function sub(a,b){
+    let c=a-b;
+console.log("Subtraction of two number is",c)
+
+if(c%2==0){
+    console.log("even")
+}
+else console.log("odd")
+}
+
+
+add(5,5);
+sub(3,7)
