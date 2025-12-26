e6c15cd0ee4e30309538fa8fa7604cf4df0e9405
Amit verma <190012191+amitskverma@users.noreply.github.com>
2025-12-26 10:08:16 +0530

    sum of an array

---
+++ sumofarray.js
@@ -0,0 +1,15 @@
+function sumofarray(ary)
+{
+    let sum=0;
+    for (let i=0;i<ary.length;i++){
+        sum=sum+ary[i];
+
+    }
+    console.log("sum of an array is"+sum);
+
+}
+
+
+ary=[1,2,3,4,5,6,7,8,9,10];
+
+sumofarray(ary);
